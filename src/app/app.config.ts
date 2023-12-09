0d37f76b1d139c0ef1c56df36a32fbff30729cec
Maurane <demaude19@gmail.com>
2023-12-09 09:41:58 +0100

    message

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
